b4e9ea9d79312fbca3a14a89b135216c1ab36704
Yasu Sato <yasufabric1192@gmail.com>
2019-06-01 16:54:11 +0900

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badg-pill badge-secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
